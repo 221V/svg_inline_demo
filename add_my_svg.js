c8117c859cb2b57cd04205d27c2b871d4014227c
221V <221v92@gmail.com>
2020-01-01 16:48:37 +0200

    code init

---
+++ add_my_svg.js
@@ -0,0 +1,16 @@
+var addmainsvg='<svg style="position: absolute; width: 0; height: 0;" width="0" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
++'<defs>'
++'<symbol id="icoellipsisv" viewBox="0 0 192 512">'
++'<title>icoellipsisv</title>'
++'<path d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z"></path>'
++'</symbol>'
++'<symbol id="icoshoppingbag" viewBox="0 0 448 512">'
++'<title>icoshoppingbag</title>'
++'<path d="M352 160v-32C352 57.42 294.579 0 224 0 153.42 0 96 57.42 96 128v32H0v272c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V160h-96zm-192-32c0-35.29 28.71-64 64-64s64 28.71 64 64v32H160v-32zm160 120c-13.255 0-24-10.745-24-24s10.745-24 24-24 24 10.745 24 24-10.745 24-24 24zm-192 0c-13.255 0-24-10.745-24-24s10.745-24 24-24 24 10.745 24 24-10.745 24-24 24z"></path>'
++'</symbol>'
++'</defs>'
++'</svg>';
+var boxforsvg=document.createElement('div');
+boxforsvg.style='display:none;';
+boxforsvg.innerHTML=addmainsvg;
+document.body.appendChild(boxforsvg);
